8b05771173b3bb1571b6e45fcbe6e1b7a201fb8e
Matt Fairbrass <matt.fairbrass@gmail.com>
2019-02-20 09:31:22 +1100

    chore: Update demo app to use render the map using the MapboxGlLayer for react-leaflet

--- src/demo/containers/app/app.jsx
+++ src/demo/containers/app/app.jsx
@@ -1,8 +1,15 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { Map } from 'react-leaflet';
+import MapboxGlLayer from 'react-mapbox-gl-leaflet';
 
 import styles from './app.scss';
+
+const mapAttribution = `
+  <a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a>
+  <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>
+`;
 
 class App extends PureComponent {
   static propTypes = {
@@ -16,14 +23,26 @@ class App extends PureComponent {
   render() {
     const { className } = this.props;
 
-    const _className = classnames(
-      styles.root,
-      className
-    );
+    const position = [-33.86785, 151.20732];
+    const _className = classnames(styles.root, className);
 
     return (
-      <div className={_className}>
-        <p>Map goes here</p>
+      <div
+        ref="mapContainer"
+        className={_className}>
+        <Map
+          className={styles.map}
+          ref="map"
+          preferCanvas
+          center={position}
+          zoom={12}
+          minZoom={0}
+          maxZoom={14}>
+          <MapboxGlLayer
+            ref="mapboxGL"
+            style="https://maps.tilehosting.com/styles/bright/style.json?key=2CLrASB61Gj6GPxI8aKe"
+            attribution={mapAttribution} />
+        </Map>
       </div>
     );
   }
